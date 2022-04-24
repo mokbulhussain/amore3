48598d31ba4ad84b847a7285a45eb40f8b7b9443
mokbulhussain <mokbulhussain2@gmail.com>
2022-04-25 04:13:44 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+(function ($) {
+    "use strict";
+
+
+
+        // testimonial-active
+        $('.client').slick({
+
+            centerPadding: '60px',
+            arrows: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows: true,
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+
+                        arrows:false
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        centerMode: true,
+                        centerPadding: '30px',
+                        infinite: true,
+                        arrows:false,
+                    }
+                }
+            ]
+        });   
+
+
+
+
+
+
+
+})(jQuery);	    
