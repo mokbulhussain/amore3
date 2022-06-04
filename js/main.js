cb9e78a317e8be7a7a2e15c7ccf2872419be2e19
mokbulhussain <mokbulhussain2@gmail.com>
2022-06-04 12:50:14 +0600

    responsive update

--- js/main.js
+++ js/main.js
@@ -18,14 +18,14 @@
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 2,
+                        slidesToShow: 3,
                         slidesToScroll: 1,
                         infinite: true,
                         arrows: true,
                     }
                 },
                 {
-                    breakpoint: 767,
+                    breakpoint: 992,
                     settings: {
                         slidesToShow: 1,
                         slidesToScroll: 1
